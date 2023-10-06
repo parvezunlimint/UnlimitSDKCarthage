f18de1172a1a2682abc8f6fe72920a8891564bff
Mohammad Parvez <m.parvez@unlimint.com>
2023-10-06 15:54:15 +0530

    Initial Commit

---
+++ NewIndicator/Carthage/Build/iOS/NewIndicator.framework/Headers/NewIndicator.h
@@ -0,0 +1,18 @@
+//
+//  NewIndicator.h
+//  NewIndicator
+//
+//  Created by Mohammad Parvez on 06/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NewIndicator.
+FOUNDATION_EXPORT double NewIndicatorVersionNumber;
+
+//! Project version string for NewIndicator.
+FOUNDATION_EXPORT const unsigned char NewIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewIndicator/PublicHeader.h>
+
+
