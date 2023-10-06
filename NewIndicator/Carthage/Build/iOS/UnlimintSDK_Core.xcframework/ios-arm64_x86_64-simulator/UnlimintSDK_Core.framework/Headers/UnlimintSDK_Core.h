411f6af2a03fb9b9783288d9bf7a7eac1fbcff65
Mohammad Parvez <m.parvez@unlimint.com>
2023-10-06 16:01:55 +0530

    sdk added

---
+++ NewIndicator/Carthage/Build/iOS/UnlimintSDK_Core.xcframework/ios-arm64_x86_64-simulator/UnlimintSDK_Core.framework/Headers/UnlimintSDK_Core.h
@@ -0,0 +1,18 @@
+//
+//  UnlimintSDK_Core.h
+//  UnlimintSDK-Core
+//
+//  Created by Denis Gnezdilov on 19.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnlimintSDK_Core.
+FOUNDATION_EXPORT double UnlimintSDK_CoreVersionNumber;
+
+//! Project version string for UnlimintSDK_Core.
+FOUNDATION_EXPORT const unsigned char UnlimintSDK_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnlimintSDK_Core/PublicHeader.h>
+
+
---
+++ NewIndicator/Carthage/Build/iOS/UnlimintSDK_SwiftUI.xcframework/ios-arm64_x86_64-simulator/UnlimintSDK_SwiftUI.framework/Headers/UnlimintSDK_SwiftUI.h
@@ -0,0 +1,18 @@
+//
+//  UnlimintSDK_SwiftUI.h
+//  UnlimintSDK-SwiftUI
+//
+//  Created by Mohammad Parvez on 20/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnlimintSDK_SwiftUI.
+FOUNDATION_EXPORT double UnlimintSDK_SwiftUIVersionNumber;
+
+//! Project version string for UnlimintSDK_SwiftUI.
+FOUNDATION_EXPORT const unsigned char UnlimintSDK_SwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnlimintSDK_SwiftUI/PublicHeader.h>
+
+
